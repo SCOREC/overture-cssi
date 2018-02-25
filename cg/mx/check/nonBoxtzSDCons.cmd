651c4d8424169f714b48f948259492d826cd92a8
jbangel <jordan.angel1729@gmail.com>
2018-02-25 12:45:30 -0500

    cmd files for regression tests

---
+++ cg/mx/check/nonBoxtzSDCons.cmd
@@ -0,0 +1,23 @@
+* 
+nonBox1.order4.ng3
+*
+NFDTD
+* 
+twilightZone
+degreeSpace, degreeTime 4 4 
+*
+bc: all=dirichlet
+*
+tFinal .2
+tPlot .1 
+dissipation 0.0 
+use sosup dissipation 1
+sosup parameter 1.
+sosup dissipation option 0
+use conservative difference 1
+* 
+continue
+* 
+movie mode
+finish
+
